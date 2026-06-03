f4542bcf1147328ebaba0e3c967c2cb55fc7eee5
Liono <lordfosterinc@gmail.com>
2026-06-03 04:28:26 -0700

    Revert "fix: add OneSignal notification prompt"
    This reverts commit f83c24ca0e6460c51f03c6c7c3a16afeafbe6e37.

--- src/main.jsx
+++ src/main.jsx
@@ -8,27 +8,8 @@ window.OneSignalDeferred = window.OneSignalDeferred || [];
 window.OneSignalDeferred.push(async function(OneSignal) {
   await OneSignal.init({
     appId: "6abae88c-4a8e-4311-908c-5079a8acf2d3",
-    notifyButton: { enable: true },
+    notifyButton: { enable: false },
     allowLocalhostAsSecureOrigin: true,
-    promptOptions: {
-      slidedown: {
-        prompts: [
-          {
-            type: "push",
-            autoPrompt: true,
-            text: {
-              actionMessage: "SafeAlertNG needs permission to send you emergency alerts and panic notifications.",
-              acceptButton: "Allow Alerts",
-              cancelButton: "No Thanks",
-            },
-            delay: {
-              pageViews: 1,
-              timeDelay: 3,
-            },
-          }
-        ]
-      }
-    }
   });
 });
 
